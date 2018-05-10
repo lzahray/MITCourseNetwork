aa0601f9b6e28141ca695bfdf5a2ad17832f100c
Brandon Wang <bdnwang@mit.edu>
2018-05-09 23:09:42 -0400

    Turn ingestCatalog.py into a function.

--- ingestCatalog.py
+++ ingestCatalog.py
@@ -3,13 +3,16 @@ import csv
 from parse_req import parse_req_string
 from prereqDataParser import Course
 
-courses = []
-with open('catalog.csv', 'rt') as csvfile: 
-    reader = csv.reader(csvfile, delimiter=",")
-    for row in reader:
-        courses.append(Course(row[2], parse_req_string(row[5]), None, row[4] == 'U'))
+def ingest_catalog():
 
+    courses = []
+    with open('catalog.csv', 'rt') as csvfile: 
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            courses.append(Course(row[2], parse_req_string(row[5]), None, row[4] == 'U'))
 
-for c in courses:
-    print(c.__repr__())
+if __name__ == '__main__':
+    course = ingest_catalog()
+    for c in courses:
+        print(c.__repr__())
 
